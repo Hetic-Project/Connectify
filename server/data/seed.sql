fbb1b81fa43a335ef2ffac515d89432a340d6622
Lucas-ylm <120712755+Lucas-ylm@users.noreply.github.com>
2023-05-09 17:11:47 +0200

    update du seed.sql, il faut que je fix les erreurs il y aura un autre commit ce soir

--- server/data/seed.sql
+++ server/data/seed.sql
@@ -6,6 +6,7 @@ BEGIN;
 -- SHOW tables;
 -- SELECT * from `group`;
 
+-- CREATE USER 'connectify' IDENTIFIED BY 'connectify';
 -- GRANT ALL PRIVILEGES ON connectify.* TO 'connectify' WITH GRANT OPTION;
 -- FLUSH PRIVILEGES;
 
@@ -30,5 +31,117 @@ INSERT INTO `group` (
     'une déscription 2',
     'privés'
 );
+
+INSERT INTO `role` (`name`) 
+VALUES ('admin');
+
+INSERT INTO `page` (
+`name`,
+'banner',
+'picture'
+
+) VALUES (
+    'admin',
+    'banner url',
+    'picture url'
+);
+
+INSERT INTO `promo` (
+`promo_name`
+
+) VALUES (
+    'PROMO WEB1 P2025'
+);
+
+INSERT INTO `user` (
+`firstname`,
+`lastname`,
+`mail`,
+`password`,
+`username`,
+`picture`,
+`banner`,
+`active`,
+`role_id`,
+`promo_id`
+
+) VALUES (
+    'VANDAL',
+    'William',
+    'williamvandal@gmail.com',
+    'vandal.william',
+    'profile picture url',
+    'banner url',
+    'compte activé',
+    '56515',
+    '1353'
+);
+
+INSERT INTO `publication` (
+`title`,
+`content`,
+`picture`,
+`author_id`
+
+) VALUES (
+    'Photo de classe',
+    'photo réalisée par : Jean Charles',
+    'picture url',
+    '284'
+);
+
+INSERT INTO `comment` (
+`comment_content`,
+`user_id`,
+`publication_id`
+
+) VALUES (
+    'PROMO WEB1 P2025',
+    '135',
+    '2651'
+);
+
+INSERT INTO `member` (
+`group_id`,
+`user_id`,
+`role_id`
+
+) VALUES (
+    '513',
+    '6513',
+    '226'
+);
+
+INSERT INTO `role_page` (
+`user_id`,
+`role_id`,
+
+) VALUES (
+    '6513',
+    '226'
+);
+
+INSERT INTO `private_message` (
+`message_content`,
+`transmitter_id`,
+`receiver_id`
+
+) VALUES (
+    'contenu du message',
+    '55',
+    '66'
+);
+
+INSERT INTO `group_message` (
+`message_content`,
+`transmitter_id`,
+`group_id`
+
+) VALUES (
+    'contenu du message',
+    '55',
+    '12'
+);
+
 
 COMMIT;
