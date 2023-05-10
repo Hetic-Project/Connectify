d79ca78da66105a2f194da1daaa1f2683bc1e3fa
Vandal-William <vandal.william@hotmail.com>
2023-05-10 15:09:33 +0200

    routing

---
+++ server/controllers/userController.php
@@ -0,0 +1,34 @@
+<?php
+
+//Inclusion du fichier pour la connexion a la BDD
+require_once './debug.php';
+require_once './database/client.php';
+
+
+// Création du controller users
+
+class User {
+
+    function getAllUsers(){
+
+        // j'appelle l'objet base de donnée
+        $db = new Database();
+
+        // je me connecte à la BDD avec la fonction getConnection de l'objet Database
+        $connexion = $db->getConnection();
+
+        // je prépare la requête
+        $request = $connexion->prepare("SELECT * FROM user");
+        // j'exécute la requête
+        $request->execute();
+        // je récupère tous les résultats dans users
+        $users = $request->fetchAll(PDO::FETCH_ASSOC);
+        // je ferme la connection
+        $connexion = null;
+
+        // je renvoie au front les données au format json
+        header('Content-Type: application/json');
+        echo json_encode($users);
+    }
+    
+}
---
+++ server/index.php
@@ -0,0 +1,7 @@
+<?php
+// Afficher les erreurs dans le navigateur
+ini_set('display_errors', 1);
+
+
+// Touts les fichiers routes de l'application
+require_once './routers/userRoutes.php';
