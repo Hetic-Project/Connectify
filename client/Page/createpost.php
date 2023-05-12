6c0362f01ec9b03b46109ac80bbf41db8358c1e7
Kevin Bernard <kevinbernard@Kevins-MacBook-Pro.local>
2023-05-12 09:14:28 +0200

    debut du creat post

--- client/Page/createpost.php
+++ client/Page/createpost.php
@@ -4,7 +4,7 @@ session_start();
 ?>
 
         <main class="main">
-                <div> <a class="bouttonRetour" href="../css/publications.php"> <img src="../asset/iconRetour.svg" alt="Bouton_Retour"> </a> </div>
+                <div> <a class="iconRetour" href="../css/publications.css"> <img src="../asset/iconRetour.svg" alt="iconRetour"> Retour</a> </div>
         </main>
 </body>
 </html>
