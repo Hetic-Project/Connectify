5548423092be9472ac838de4c70eb52a0d80727b
Nioctalü <a_boccheciampe@hetic.eu>
2023-05-15 09:41:12 +0200

    page une publication

--- client/Page/publications.php
+++ client/Page/publications.php
@@ -38,22 +38,7 @@ session_start();
                                 <button class="invitationButton textWhite green">Rejuser</button>
                         </div>
                 </div>
-                <div class="containerPublication">
-                        <button class="buttonPublication">
-                                <div class="containerProfile">
-                                        <img class="imageProfile" src="">
-                                        <div class="profile">
-                                                <h3 class="textWhite">Pseudo</h3>
-                                                <p class="textGray">Promo</p>
-                                                <p class="textGray">Maintenant</p>
-                                        </div>
-                                </div>
-                                <h3 class="titrePublication textWhite">Je suis un titre qui ne sert à rien</h3>
-                                <img class="imagePublication" src="../asset/Daniel Balavoine.jpg">
-                        </button>
-
-
-                </div>
+                <?php include "containerpublication.php" ?>
         </main>
 </body>
 </html> 
