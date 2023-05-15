bae58e44419db0d0d9ffd57b694da71b3fdac4c0
Rubens Bonnin <r_bonnin@hetic.eu>
2023-05-15 11:37:32 +0200

    renommé balise p login

--- client/Page/login.php
+++ client/Page/login.php
@@ -8,7 +8,7 @@ session_start();
         <body>
                 <div class="wrapper">
                         <h1>Connexion</h1>
-                        <p>Utilisez les identifiants fournis par votre établissement</p>
+                        <p class="textLogin">Utilisez les identifiants fournis par votre établissement</p>
                         <form>
                                 <input type="text" placeholder="Enter username">
                                 <input type="password" placeholder="Password">
