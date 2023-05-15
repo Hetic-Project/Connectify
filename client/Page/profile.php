f00c015cf5abb2aa80c8fecea0882e67854aed33
Rubens Bonnin <r_bonnin@hetic.eu>
2023-05-15 14:36:34 +0200

    MAJ Profile ajout fonction JS (SWITCH)

--- client/Page/profile.php
+++ client/Page/profile.php
@@ -8,19 +8,33 @@ session_start();
     <img class="banniereProfil" src="<?= $gcef ?>">
     </img>
 
-    <img class="PP" src="<?= $gcef ?>"></img>
-    <btn class="modifierProfil"> Modifier le Profil</btn>
+    <div class="profileInvitation">
+        <img src="../asset/IconProfile.svg" alt="Image de profile" class="imageProfile">
+        <div class="nomPromo">
+            <h3 class="textWhite">Rubens Bonnin</h3>
+            <p class="textGray">Promo</p>
+        </div>
+        <btn class="modifierProfil"> Modifier le Profil</btn>
+    </div>
+    <br>
+    <div class="Description">
+        <h4 class="textWhite">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. </h4>
+        <br>
 
-    <? "description"
-        ?>
-
-
-
-
-    <div>
+        <p class="textGray"> 42 abonnés 666 abonnements</p>
     </div>
 
+    <div class="form-box">
+        <div class="button-box">
+            <div id="button1"></div>
+            <button type="button" class="toggle-button1" onclick="leftClick()">Left</button>
+            <button type="button" class="toggle-button1" onclick="rightClick()">Right</button>
+        </div>
+    </div>
 </main>
+<script src="../js/script.js"></script>
 </body>
 
 </html>
