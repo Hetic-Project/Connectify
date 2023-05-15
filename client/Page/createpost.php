0778f3222a7453eff62666b8648cb1fde651b6b3
Kevin Bernard <kevinbernard@Kevins-MacBook-Pro.local>
2023-05-15 11:11:56 +0200

    ratio

--- client/Page/createpost.php
+++ client/Page/createpost.php
@@ -4,7 +4,8 @@ session_start();
 ?>
 
         <main class="main">
-                <a class="iconRetour" href="../page/publications.php"> <img src="../asset/iconRetour.svg" alt="iconRetour"> Retour</a>
+                <a class="iconRetour" href="../page/publications.php"> <img src="../asset/iconRetour.svg" alt="iconRetour"> <button class="buttonRetour">Retour</button></a>
+                <button class="buttonPublier">PUBLIER</button>
         </main>
 </body>
 </html>
