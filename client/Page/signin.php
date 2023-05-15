02f67daac108d9c289c061998c96b326bd95b85d
Rubens Bonnin <r_bonnin@hetic.eu>
2023-05-15 11:26:22 +0200

    MAJ signin

--- client/Page/signin.php
+++ client/Page/signin.php
@@ -13,7 +13,7 @@ session_start();
                 <input type="text" placeholder="Enter username">
                 <input type="password" placeholder="Password">
             </form>
-
+            <button class="Signin">Sign in</button>
         </div>
     </body>
 </main>
