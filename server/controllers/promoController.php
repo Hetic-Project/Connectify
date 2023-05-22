3bf3cb3ba71fdd91ae8f149146c3ee34952e760a
khalillamiaa <127410042+khalillamiaa@users.noreply.github.com>
2023-05-22 14:26:46 +0200

    controller

--- server/controllers/promoController.php
+++ server/controllers/promoController.php
@@ -1,33 +1,118 @@
-<?php
-
+<!-- <?php 
+// 
 //Inclusion du fichier pour la connexion a la BDD
 require_once './debug.php';
 require_once './database/client.php';
 
-
-// Création du controller users
-
 class Promo {
 
-    function getAllPromos (){
 
+    // méthode pour récupérer toutes les promotions
+    function getAllPromos() {
+        $sql = "SELECT * FROM promos";
+        $result = $this->conn->query($sql);
+        if ($result->num_rows > 0) {
+            $promos = array();
+            while($row = $result->fetchAll()) {
+                $promos[] = $row;
+            }
+            return $promos;
+        }
+         else {
+            return null;
+        }
     }
-
     function getOnePromo ($id_promo){
+        $sql = "SELECT * FROM promos WHERE id_promo = '$id_promo'";
+        $result = $this->conn->query($sql);
+        if ($result->num_rows > 0) {
+            $promo = $result->fetch();
+            return $promo;
+        } else {
+            return null;
+          }
         
     }
+    function addPromoIfAdmin ($id_promo){
 
-    function addPromoIfAdmin (){
-        $_SESSION['id'];
+            // ajouter les variables dans les parametres de la fonction
+       
+            // Vérifier que l'utilisateur est connecté et qu'il est un administrateur
+            session_start();
+            if(isset($_SESSION['id']) && $_SESSION['is_admin'] === true) {
+                // Ajouter la promotion à la base de données
+                $sql = "INSERT INTO promos (promo_name, page_id, group_id, description) VALUES (:promo_name, :page_id, :group_id, :description)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(':promo_name', $promo_name)e;
+        $stmt->bindValue(':description', $description);
+        $stmt->bindValue(':page_id', $page_id);
+        $stmt->bindValue(':group_id', $group_id);
+        $stmt->execute();
+        
+            } else {
+                // Renvoyer un message d'erreur
+                echo "Vous n'êtes pas autorisé à effectuer cette action.";
+            }
+
     }
-
     function deletePromoIfAdmin ($id_promo){
-        $_SESSION['id'];
+        
+            // Vérifier que l'utilisateur est connecté et qu'il est un administrateur
+            session_start();
+            if(isset($_SESSION['id']) && $_SESSION['is_admin'] === true) {
+                // Supprimer la promotion de la base de données 
+                $sql = "DELETE FROM promos WHERE id_promo = :id_promo";
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute(['id_promo' => $id_promo]);
+
+                // connexion à la base de données
+                $conn = new mysqli('localhost', 'user', 'motdepasse', 'ma_base_de_donnees');
+                
+                // Vérification de la connexion
+                if ($conn->connect_error) {
+                    die("La connexion a échoué: " . $conn->connect_error);
+                } 
+                
+                // Requête de suppression
+                // $sql = "DELETE FROM promotions WHERE id_promo = id_promo";
+                // $stmt = $conn->prepare($sql);
+                // $stmt->execute();
+                // Vérification de la suppression
+                if ($stmt->affected_rows > 0) {
+                    echo "La promotion a été supprimée avec succès.";
+                } else {
+                    echo "La promotion n'a pas été trouvée dans la base de données.";
+                }
+                // Fermeture de la connexion
+                $stmt->close();
+                $conn->close();
+            } else {
+                // Renvoyer un message d'erreur
+                echo "Vous n'êtes pas autorisé à effectuer cette action.";
+            }
     }
 
-    function updatePromoIfAdmin ($id_promo){
-        $_SESSION['id'];
-    }
-   
+    function updatePromoIfAdmin ($id_promo, $promoData){
+        
+            // Vérifier que l'utilisateur est connecté et qu'il est un administrateur
+            session_start();
+            if(isset($_SESSION['id']) && $_SESSION['is_admin'] === true) {
+                // Mettre à jour la promotion dans la base de données
+                $db = new PDO('mysql:host=localhost;dbname=ma_base_de_donnees', 'nom_utilisateur', 'mot_de_passe');
+                $query = "UPDATE promos SET nom = :nom, description = :description, date_debut = :date_debut, date_fin = :date_fin WHERE id_promo = :id_promo";
+                $stmt = $db->prepare($query);
+               // ajouter un bind value pour chaque paramatre dans la query ainsi que lajouter dans les parametre de la fonction
+
+                $stmt->execute();
+                if($stmt->rowCount() > 0) {
+                    echo "La promotion a été mise à jour avec succès.";
+                } else {
+                    echo "La promotion n'a pas été trouvée dans la base de données.";
+                }
+            } else {
+                // Renvoyer un message d'erreur
+                echo "Vous n'êtes pas autorisé à effectuer cette action.";
+            }
     
-}
+} -->
+// 
--- server/controllers/rubricController.php
+++ server/controllers/rubricController.php
@@ -8,22 +8,103 @@ require_once './database/client.php';
 // Création du controller users
 
 class Rubric {
-
     function getRubricsForOnePage ($id_page){
 
+        $db = new Database();
+        $conn = $db->getConnection();
+        // requete la table rubric pour récupeérer toutes les rubriques d'une page (id_page)
+        $sql = "SELECT * FROM rubric WHERE id_page = :id_page";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([':id_page' => $id_page]);
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        header ('content - type:application/json');
+        echo json_encode($result); 
     }
 
     function addOneRubricForOnePage ($id_page){
-        
-    }
-   
+    // 1. je récupère les champs du formulaire
+    $titre = $_POST['titre'];
+    $contenu = $_POST['contenu'];
+    $picture = $_POST['picture'];
+    $banner = $_POST['banner'];
+    // 2. Vérifier que l'utilisateur est connecté et qu'il est un administrateur
+    session_start();
+    $user_id = $_SESSION['user']['id'];
+    $dsn = "mysql:host=localhost;dbname=mydatabase;charset=utf8mb4";
+    $username = "username";
+    $password = "password";
+    $pdo = new PDO($dsn, $username, $password);
+    $sql = "SELECT role.name FROM role JOIN role_page ON role.id = role_page.role_id WHERE role_page.user_id = :user_id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['user_id' => $user_id]);
+         // 3. Ajouter la nouvelle rubrique dans la base de données
+         $sql = "INSERT INTO rubric (titre, contenu, id_page, picture, banner) VALUES ('$titre', '$contenu', $id_page, '$picture', '$banner')";
+         $stmt = $pdo->prepare($sql);
+         $stmt->execute(['user_id' => $user_id]);
+         // 4. Rediriger l'utilisateur vers la page des rubriques
+        header("Location: rubriques.php");
+        exit;
+            // créer une variable qui contient l'id de l'utilisateur
+            session_start();
+            $user_id = $_SESSION['user']['id'];
+            // requéter le role_id de la table role_page  join avec l'id de la table role de la base de donnée
+            SELECT role.name 
+            FROM role 
+            JOIN role_page ON role.id = role_page.role_id
+            WHERE role_page.user_id = :user_id
+                // renvoyer une réponse pour prévenir l'utilisateur du succès de l'ajout de la rubric
+                $response = array(
+                    'success' => true,
+                    'message' => 'Rubric added successfully!'
+                );
+                echo json_encode($response);
+                // Renvoyer un message d'erreur, en json
+                $response = array(
+                    'success' => false,
+                    'message' => 'You are not authorized to add a rubric.'
+                );
+                echo json_encode($response);
+                
+    }           
     function deleteRubricsForOnePage ($id_rubric){
-        $_SESSION['id'];
-
+            // Vérifier que l'utilisateur est bien connecté et est un administrateur
+            if (!isset( $_SESSION['user']['id'];) || $_SESSION['role'] !== 'admin') {
+                return json_encode(['error' => 'Unauthorized access']);
+            }
+            // Créer une instance de la classe Rubric pour accéder aux méthodes permettant de manipuler les rubriques
+            $rubricModel = new Rubric();
+            // Récupérer la rubrique spécifiée par $id_rubric
+            $rubric = $rubricModel->getRubricById($id_rubric);
+            // Vérifier que la rubrique existe
+            if (!$rubric) {
+                return json_encode(['error' => 'Rubric not found']);
+            }
+            // Supprimer la rubrique
+            $rubricModel->deleteRubricById($id_rubric);
+            // Retourner un message de succès
+            return json_encode(['message' => 'Rubric deleted successfully']);
     }
     
     function updateRubricsForOnePage ($id_rubric){
-        $_SESSION['id'];
+    
+            // Vérifier que l'utilisateur est bien connecté et est un administrateur
+            if (!isset( $_SESSION['user']['id'];) || $_SESSION['role'] !== 'admin') {
+                return json_encode(['error' => 'Unauthorized access']);
+            }
+            // Créer une instance de la classe Rubric pour accéder aux méthodes permettant de manipuler les rubriques
+            $rubricModel = new Rubric();
+            // Récupérer la rubrique spécifiée par $id_rubric
+            $rubric = $connexion->prepare("SELECT * FROM_rubric");
+            // Vérifier que la rubrique existe
+            if (!$rubric) {
+                return json_encode(['error' => 'Rubric not found']);
+            }
+            // Supprimer la rubrique
+            $rubricModel->deleteRubricById($id_rubric);
+            // Retourner un message de succès
+            return json_encode(['message' => 'Rubric deleted successfully']);
+       
+        
     }
     
 }
