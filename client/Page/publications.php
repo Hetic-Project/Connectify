54dca48fc5fb0d5bb646681bd55fc16489fcba22
Rubens Bonnin <r_bonnin@hetic.eu>
2023-05-22 15:49:46 +0200

    boutton vers recherche is ok

--- client/Page/publications.php
+++ client/Page/publications.php
@@ -9,8 +9,7 @@ session_start();
                 <h2 class="textWhite">Feeds</h2>
 
                 <div class="iconPublication">
-                        <button class="iconsButton" id="buttonRecherche"><img src="../asset/iconChercher.svg"
-                                        alt="Rechercher"></button>
+                        <a href="/Page/recherche.php"><img src="../asset/iconChercher.svg" alt="Rechercher"></a>
                         <a href="/Page/createpost.php"><img src="../asset/icon+.svg" alt="Publier un nouveau post"></a>
                 </div>
         </div>
@@ -18,27 +17,29 @@ session_start();
         <div class="demandeInvitation">
 
                 <div class="profileButtonInvitation">
-                        <button class="iconsButton" id="buttonInvitationGauche"><img src="../asset/iconRetour.svg"></button>
+                        <button class="iconsButton" id="buttonInvitationGauche"><img
+                                        src="../asset/iconRetour.svg"></button>
 
-                <div class="test15">
-                        <div class="profileInvitation">
-                                <img src="../asset/IconProfile.svg" alt="Image de profile" class="imageProfile">
+                        <div class="test15">
+                                <div class="profileInvitation">
+                                        <img src="../asset/IconProfile.svg" alt="Image de profile" class="imageProfile">
 
-                                <div class="nomPromo">
-                                        <h3 class="textWhite">Tom Cardonnel</h3>
-                                        <p class="textGray">Promo</p>
+                                        <div class="nomPromo">
+                                                <h3 class="textWhite">Tom Cardonnel</h3>
+                                                <p class="textGray">Promo</p>
+                                        </div>
+                                </div>
+                                <div class="containerInvitationButton">
+                                        <button class="invitationButton textWhite red">Refuser</button>
+                                        <button class="invitationButton textWhite green">Accepter</button>
                                 </div>
                         </div>
-                        <div class="containerInvitationButton">
-                                <button class="invitationButton textWhite red">Refuser</button>
-                                <button class="invitationButton textWhite green">Accepter</button>
-                        </div>
-                </div>
 
-                        
-                <button class="iconsButton" id="buttonInvitationDroit"><img src="../asset/iconRetour.svg"></button>
-        </div>
-        <?php include "containerpublication.php" ?>
+
+                        <button class="iconsButton" id="buttonInvitationDroit"><img
+                                        src="../asset/iconRetour.svg"></button>
+                </div>
+                <?php include "containerpublication.php" ?>
 
 </main>
 </body>
