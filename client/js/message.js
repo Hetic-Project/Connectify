8149b3e89e430c59d3ae003c6c2c3128e4709138
TomuCard <120178316+TomuCard@users.noreply.github.com>
2023-05-22 14:29:06 +0200

    update du front pour le lien d'affichage des messages

--- client/js/message.js
+++ client/js/message.js
@@ -1,28 +1,18 @@
-// Fonction pour update le form pour afficher les messages dans la BDD
 function updateMessages() {
-    // on fait un AJAX request pour récuperer les messages
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:4000/message/1/2/', true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            // La requête a marché update du form avec les nouveaux messages
-            var messages = JSON.parse(xhr.responseText);
-            var messageForm = document.getElementById('messageForm');
-            var messageContainer = messageForm.getElementsByClassName('userMessage')[0];
 
-            // Ajout de nouveaux messages au form
-            messages.forEach(function (message) {
-                var h3 = document.createElement('h3');
-                h3.textContent = message.firstname;
-                var p = document.createElement('p');
-                p.textContent = message.message_content;
-                messageContainer.appendChild(h3);
-                messageContainer.appendChild(p);
-            });
-        }
-    };
-    xhr.send();
+    fetch('http://localhost:4000/message', {
+        method: 'POST'
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Le JSON a été récupéré et converti en un objet JavaScript
+        console.log(data);
+        // Effectuez les opérations souhaitées sur les données ici
+    })
+    .catch(error => {
+        // En cas d'erreur lors de la requête
+        console.error('Erreur lors de la récupération du JSON:', error);
+    });
 }
 
-// Polling du message toutes les 5 secondes.
-setInterval(updateMessages, 1000);
+  setInterval(updateMessages, 5000);
