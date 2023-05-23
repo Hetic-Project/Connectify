924709d9aad3e2093a0fc5f72677ef4826d5b1cf
TomuCard <120178316+TomuCard@users.noreply.github.com>
2023-05-23 15:34:55 +0200

    publication bug

--- client/Page/1publication.php
+++ client/Page/1publication.php
@@ -1,299 +1,51 @@
-<<<<<<< HEAD
 <?php
 require_once '../TPL/header.php';
 session_start();
 ?>
 
         <main class="main">
+
+                <div class="onePublication">
+                        <?php include "containerpublication.php" ?>
+                </div>
                 
-                <?php include "containerpublication.php" ?>
                 
                 <h2 class="Commentaire textWhite">Commentaire</h2>
 
-                <div class="contentCommentaire">
-                <div class="testert">
-                                <div class="containerProfile">
-                                        <img class="imageProfile" src="">
-                                        <div class="profile">
-                                                <h3 class="textWhite">Tom</h3>
-                                                <p class="textGray">Promo</p>
-                                                <p class="textGray">Maintenant</p>
+                <div class="hube">
+                        <div class="contentCommentaire">
+                                <div class="oneComment">
+                                        <div class="containerProfile">
+                                                <img class="imageProfile" src="">
+                                                <div class="profile">
+                                                        <h3 class="textWhite">Tom</h3>
+                                                        <p class="textGray">Promo</p>
+                                                        <p class="textGray">Maintenant</p>
+                                                </div>
                                         </div>
+                                        <h3 class="leCommentaire textWhite">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
+                                        </h3>
                                 </div>
-
-                                <h3 class="leCommentaire textWhite">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
-                                </h3>
+                                
+                                <div class="oneComment">
+                                        <div class="containerProfile">
+                                                <img class="imageProfile" src="">
+                                                <div class="profile">
+                                                        <h3 class="textWhite">Tom</h3>
+                                                        <p class="textGray">Promo</p>
+                                                        <p class="textGray">Maintenant</p>
+                                                </div>
+                                        </div>
+                                        <h3 class="leCommentaire textWhite">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
+                                        </h3>
+                                </div>
+                                
                         </div>
                 </div>
         
         </main>
 
 </body>
-=======
-<?php
-require_once '../TPL/header.php';
-session_start();
-?>
-
-        <main class="main">
-        <?php include "containerpublication.php" ?>
-                <h2 class="Commentaire textWhite">
-                        Commentaire
-                </h2>
-
-                <div class="contentCommentaire">
-                        <div>
-                                <div class="containerProfile">
-                                        <img class="imageProfile" src="">
-                                        <div class="profile">
-                                                <h3 class="textWhite">Tom</h3>
-                                                <p class="textGray">Promo</p>
-                                                <p class="textGray">Maintenant</p>
-                                        </div>
-                                </div>
-
-                                <h3 class="leCommentaire textWhite">
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                </h3>
-
-                        </div>
-                        <div>
-                                <div class="containerProfile">
-                                        <img class="imageProfile" src="">
-                                        <div class="profile">
-                                                <h3 class="textWhite">Tom</h3>
-                                                <p class="textGray">Promo</p>
-                                                <p class="textGray">Maintenant</p>
-                                        </div>
-                                </div>
-
-                                <h3 class="leCommentaire textWhite">
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                </h3>
-
-                        </div>
-                        <div>
-                                <div class="containerProfile">
-                                        <img class="imageProfile" src="">
-                                        <div class="profile">
-                                                <h3 class="textWhite">Tom</h3>
-                                                <p class="textGray">Promo</p>
-                                                <p class="textGray">Maintenant</p>
-                                        </div>
-                                </div>
-
-                                <h3 class="leCommentaire textWhite">
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                </h3>
-
-                        </div>
-                        <div>
-                                <div class="containerProfile">
-                                        <img class="imageProfile" src="">
-                                        <div class="profile">
-                                                <h3 class="textWhite">Tom</h3>
-                                                <p class="textGray">Promo</p>
-                                                <p class="textGray">Maintenant</p>
-                                        </div>
-                                </div>
-
-                                <h3 class="leCommentaire textWhite">
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                </h3>
-
-                        </div>
-                        <div>
-                                <div class="containerProfile">
-                                        <img class="imageProfile" src="">
-                                        <div class="profile">
-                                                <h3 class="textWhite">Tom</h3>
-                                                <p class="textGray">Promo</p>
-                                                <p class="textGray">Maintenant</p>
-                                        </div>
-                                </div>
-
-                                <h3 class="leCommentaire textWhite">
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                </h3>
-
-                        </div>
-                        <div>
-                                <div class="containerProfile">
-                                        <img class="imageProfile" src="">
-                                        <div class="profile">
-                                                <h3 class="textWhite">Tom</h3>
-                                                <p class="textGray">Promo</p>
-                                                <p class="textGray">Maintenant</p>
-                                        </div>
-                                </div>
-
-                                <h3 class="leCommentaire textWhite">
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                </h3>
-
-                        </div>
-                        <div>
-                                <div class="containerProfile">
-                                        <img class="imageProfile" src="">
-                                        <div class="profile">
-                                                <h3 class="textWhite">Tom</h3>
-                                                <p class="textGray">Promo</p>
-                                                <p class="textGray">Maintenant</p>
-                                        </div>
-                                </div>
-
-                                <h3 class="leCommentaire textWhite">
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                </h3>
-
-                        </div>
-                        <div>
-                                <div class="containerProfile">
-                                        <img class="imageProfile" src="">
-                                        <div class="profile">
-                                                <h3 class="textWhite">Tom</h3>
-                                                <p class="textGray">Promo</p>
-                                                <p class="textGray">Maintenant</p>
-                                        </div>
-                                </div>
-
-                                <h3 class="leCommentaire textWhite">
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                </h3>
-
-                        </div>
-                        <div>
-                                <div class="containerProfile">
-                                        <img class="imageProfile" src="">
-                                        <div class="profile">
-                                                <h3 class="textWhite">Tom</h3>
-                                                <p class="textGray">Promo</p>
-                                                <p class="textGray">Maintenant</p>
-                                        </div>
-                                </div>
-
-                                <h3 class="leCommentaire textWhite">
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                </h3>
-
-                        </div>
-                        <div>
-                                <div class="containerProfile">
-                                        <img class="imageProfile" src="">
-                                        <div class="profile">
-                                                <h3 class="textWhite">Tom</h3>
-                                                <p class="textGray">Promo</p>
-                                                <p class="textGray">Maintenant</p>
-                                        </div>
-                                </div>
-
-                                <h3 class="leCommentaire textWhite">
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                </h3>
-
-                        </div>
-                        <div>
-                                <div class="containerProfile">
-                                        <img class="imageProfile" src="">
-                                        <div class="profile">
-                                                <h3 class="textWhite">Tom</h3>
-                                                <p class="textGray">Promo</p>
-                                                <p class="textGray">Maintenant</p>
-                                        </div>
-                                </div>
-
-                                <h3 class="leCommentaire textWhite">
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                </h3>
-
-                        </div>
-                        <div>
-                                <div class="containerProfile">
-                                        <img class="imageProfile" src="">
-                                        <div class="profile">
-                                                <h3 class="textWhite">Tom</h3>
-                                                <p class="textGray">Promo</p>
-                                                <p class="textGray">Maintenant</p>
-                                        </div>
-                                </div>
-
-                                <h3 class="leCommentaire textWhite">
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                        DescriptionDescriptionDescriadadada<br>
-                                </h3>
-
-                        </div>
-                </div>
-
-                
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-        </main>
-
-</body>
->>>>>>> 5273bcb8d18bc9cf3d4cf4f97c0e4a9b19a564f5
 </html>
