920a0ae340832d6bbe5a748f5d6ef01999dc2f2f
Kevin Bernard <kevinbernard@Kevins-MacBook-Pro.local>
2023-05-23 10:56:54 +0200

    creatpost fini + 50% du creatGroupe

--- client/Page/creategroup.php
+++ client/Page/creategroup.php
@@ -4,7 +4,24 @@ session_start();
 ?>
 
         <main class="main">
+        <a class="iconRetour" href="../page/message.php"> <img src="../asset/iconRetour.svg" alt="iconRetour"> <button class="buttonRetour">Retour</button></a>
+        <form>
+                <button class="buttonCréerGroupe1">CRÉER</button>
+                <div class="profileCreatGroupe">
+                        <img src="../asset/IconProfile.svg" alt="Image de profile creat post" class="imageProfileCreatPost">
 
+                        <div class="nomPromoCreatGroupe">
+                                <h3 class="textWhite">Tom Cardonnel</h3>
+                                <p class="textGray">Promo</p>
+                        </div>
+                </div>
+                <div class="ajouterUnTitre">    
+                        <input class="textWhite inputName" type="text" placeholder="Nom du Groupe">
+                </div>
+                <textarea class="textWhite inputDescritpion" type="text" placeholder="Description du Groupe"></textarea>
+                
+                <button class="buttonCréerGroupe2">CRÉER</button>
+        </form>
         </main>
 </body>
 </html>
