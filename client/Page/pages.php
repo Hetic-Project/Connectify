b37722478d74ecb7a54db06d340f17666c113ed4
khalillamiaa <127410042+khalillamiaa@users.noreply.github.com>
2023-05-25 09:25:40 +0200

    code lamiaa 2

--- client/Page/pages.php
+++ client/Page/pages.php
@@ -3,8 +3,21 @@ require_once '../TPL/header.php';
 session_start();
 ?>
 
-        <main class="main">
+<main class="main">
 
-        </main>
+        <div class="headerPublication">
+                <h2 class="textWhite">Name Group</h2>
+
+                <div class="iconPublication">
+                        <a href="/Page/modifiergroup.php"><img src="../asset/iconSetting.svg" alt="Paramettre"></a>
+                        <a href="/Page/createpost.php"><img src="../asset/icon+.svg" alt="Publier un nouveau post"></a>
+                </div>
+        </div>
+
+        
+        <?php include "containerpublication.php" ?>
+
+</main>
 </body>
+
 </html>
